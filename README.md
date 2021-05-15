f92f46c79d53b2f3e59fdb858279367b72ce1d5d
Richard <samedii@gmail.com>
2021-05-15 20:08:19 +0200

    improve: mocks of crags, sectors, and images

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## Build
 
-### Locally
+Images will be built and pushed by github actions.
+
+### Local
 
 ```bash
 docker build . -t opencrags-rest-api
 ```
-
-## Run
 
 ```bash
 docker run -p 0.0.0.0:8000:8000 opencrags-rest-api
@@ -16,6 +16,32 @@ docker run -p 0.0.0.0:8000:8000 opencrags-rest-api
 
 ## Development
 
+### Install
+
+#### Recommended
+Install pyenv
+```bash
+curl https://pyenv.run | bash
+```
+
+Install python 3.8
+```bash
+pyenv install 3.8.8
+```
+
+#### Install with poetry
+Install poetry
+```bash
+pyenv local 3.8.8
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+```
+
+Install python dependencies
+```bash
+poetry install
+```
+
+### Development server
 ```bash
 uvicorn app.main:app --reload
 ```
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.testclient import TestClient
 from starlette.responses import RedirectResponse
@@ -7,15 +7,15 @@ import io
 from uuid import UUID
 import base64
 import json
-from pathlib import Path
 import PIL
 from pydantic import BaseModel, validator, Field
+from enum import Enum
 from typing import List, Union, Literal, Optional
 
 
 app = FastAPI(
     title="opencrags-rest-api",
-    description="Database of lead climbs and boulder problems",
+    description="Database of boulder problems and routes",
     version="0.0.0",
 )
 
@@ -25,8 +25,122 @@ def redirect_to_docs():
     return RedirectResponse(url="/docs")
 
 
-class Image(BaseModel):
-    base64_image: Optional[str] = Field(..., example="/9j/4AAQSkZJRgAB...")
+class Crag(BaseModel):
+    name: str
+    location_polygon: str
+
+class CragId(BaseModel):
+    crag_id: UUID = Field(..., alias="id")
+
+@app.post(
+    "/crags",
+    response_model=CragId,
+    status_code=status.HTTP_201_CREATED,
+    tags=["crags"],
+)
+def add_crag(crag: Crag):
+    crag_id = UUID(int=random.getrandbits(128))
+    return CragId(id=crag_id)
+
+
+@app.put(
+    "/crags/{crag_id}",
+    response_model=Crag,
+    status_code=status.HTTP_200_OK,
+    tags=["crags"],
+)
+def update_crag(crag_id: UUID, crag: Crag):
+    crag_id = UUID(int=random.getrandbits(128))
+    return CragId(id=crag_id)
+
+
+@app.delete(
+    "/crags/{crag_id}",
+    status_code=status.HTTP_200_OK,
+    tags=["crags"],
+)
+def remove_crag(
+    crag_id: UUID,
+):
+    return Response(status_code=status.HTTP_200_OK)
+
+
+
+class IdentifiedCrag(CragId, Crag):
+    pass
+
+@app.get(
+    "/crags/{crag_id}",
+    response_model=IdentifiedCrag,
+    status_code=status.HTTP_200_OK,
+    tags=["crags"],
+)
+def view_crag():
+    pass
+
+
+class Sector(BaseModel):
+    name: str
+    crag_id: UUID
+
+class SectorId(BaseModel):
+    sector_id: UUID = Field(..., alias="id")
+
+@app.post(
+    "/crags/{crag_id}/sectors",
+    response_model=Crag,
+    status_code=status.HTTP_201_CREATED,
+    tags=["crags/sectors"],
+)
+def add_sector_to_crag(crag_id: UUID, sector: Sector):
+    sector_id = UUID(int=random.getrandbits(128))
+    return SectorId(id=sector_id)
+
+
+@app.put(
+    "/crags/{crag_id}/sectors/{sector_id}",
+    response_model=Crag,
+    status_code=status.HTTP_201_CREATED,
+    tags=["crags/sectors"],
+)
+def update_sector(crag_id: UUID, sector_id: UUID, sector: Sector):
+    return SectorId(id=sector_id)
+
+
+@app.delete(
+    "/crags/{crag_id}/sectors/{sector_id}",
+    status_code=status.HTTP_200_OK,
+    tags=["crags/sectors"],
+)
+def remove_sector_from_crag(
+    crag_id: UUID,
+    sector_id: UUID,
+):
+    return Response(status_code=status.HTTP_200_OK)
+
+
+class IdentifiedSector(SectorId, Sector):
+    pass
+
+@app.get(
+    "/crags/{crag_id}/sectors/{sector_id}",
+    response_model=List[IdentifiedSector],
+    status_code=status.HTTP_200_OK,
+    tags=["crags/sectors"],
+)
+def view_sector(
+    crag_id: UUID,
+    sector_id: UUID,
+):
+    pass
+
+
+class ImageInfo(BaseModel):
+    location: str
+    sector_id: Optional[UUID]
+
+class Image(ImageInfo):
+    base64_image: str = Field(..., example="/9j/4AAQSkZJRgAB...")
 
     def image(self):
         return PIL.Image.open(
@@ -35,24 +149,100 @@ class Image(BaseModel):
             )
         )
 
-class Climb(Image):
+class ImageId(BaseModel):
+    image_id: UUID = Field(..., alias="id")
+
+@app.post(
+    "/images",
+    response_model=ImageId,
+    status_code=status.HTTP_201_CREATED,
+    tags=["images"],
+)
+def add_image(
+    image: Image,
+):
+    image_id = UUID(int=random.getrandbits(128))
+    return ImageId(id=image_id)
+
+
+@app.put(
+    "/images/{image_id}",
+    status_code=status.HTTP_200_OK,
+    tags=["images"],
+)
+def update_image_info(
+    image_id: UUID,
+    image_info: ImageInfo,
+):
+    pass
+
+
+@app.delete(
+    "/images/{image_id}",
+    status_code=status.HTTP_200_OK,
+    tags=["images"],
+)
+def remove_image(
+    image_id: UUID,
+):
+    return Response(status_code=status.HTTP_200_OK)
+
+
+class IdentifiedImage(Image, ImageId):
+    pass
+
+@app.get(
+    "/images/{image_id}",
+    response_model=IdentifiedImage,
+    status_code=status.HTTP_200_OK,
+    tags=["images"],
+)
+def view_image(
+    image_id: UUID,
+):
+    pass
+
+
+class Climb(BaseModel):
+    name: str
+    sector_id: UUID
+
+class ClimbId(BaseModel):
     climb_id: UUID = Field(..., alias="id")
 
 @app.post(
     "/climbs",
     response_model=Climb,
     status_code=status.HTTP_201_CREATED,
+    tags=["climbs"],
 )
 def add_climb(
-    image: Image,
+    climb: Climb,
 ):
     climb_id = UUID(int=random.getrandbits(128))
-    return Climb(id=climb_id)
+    return ClimbId(id=climb_id)
+
+
+class IdentifiedClimb(ClimbId, Climb):
+    pass
+
+@app.put(
+    "/climbs/{climb_id}",
+    response_model=IdentifiedClimb,
+    status_code=status.HTTP_200_OK,
+    tags=["climbs"],
+)
+def update_climb(
+    climb_id: UUID,
+    climb: Climb,
+):
+    pass
 
 
 @app.delete(
     "/climbs/{climb_id}",
     status_code=status.HTTP_200_OK,
+    tags=["climbs"],
 )
 def remove_climb(
     climb_id: UUID,
@@ -62,60 +252,62 @@ def remove_climb(
 
 @app.get(
     "/climbs/{climb_id}",
-    response_model=Climb,
+    response_model=IdentifiedClimb,
     status_code=status.HTTP_200_OK,
+    tags=["climbs"],
 )
 def view_climb(
     climb_id: UUID,
 ):
-    climb_id = UUID(int=random.getrandbits(128))
-    return Climb(id=climb_id)
-
-
-class CragInfo(BaseModel):
-    name: str
-
-class Crag(CragInfo):
-    crag_id: UUID = Field(..., alias="id")
-
-@app.post(
-    "/crags/{crag_id}",
-    response_model=Crag,
-    status_code=status.HTTP_201_CREATED,
-)
-def add_crag(crag_info: CragInfo):
-    crag_id = UUID(int=random.getrandbits(128))
-    return Crag(
-        id=crag_id,
-        name=crag_info.name,
-    )
-
-
-@app.delete(
-    "/crags/{crag_id}",
-    status_code=status.HTTP_200_OK,
-)
-def remove_crag(
-    crag_id: UUID,
-):
-    return Response(status_code=status.HTTP_200_OK)
-
-
-@app.get(
-    "/crags",
-    response_model=List[Crag],
-    status_code=status.HTTP_200_OK,
-)
-def list_crags():
     pass
 
 
-@app.post(
+class ClimbType(str, Enum):
+    any = "any"
+    boulder = "boulder"
+    route = "route"
+
+class SortBy(str, Enum):
+    distance = "distance"
+    grade = "grade"
+    stars = "stars"
+    ascents = "ascents"
+
+class SortOrder(str, Enum):
+    ascending = "ascending"
+    descending = "descending"
+
+@app.get(
+    "/climbs",
+    response_model=List[Climb],
+    status_code=status.HTTP_200_OK,
+    tags=["climbs"],
+)
+def advanced_search_for_climbs(
+    type: ClimbType,
+    min_grade: str = "4",
+    max_grade: str = "9C",
+    min_stars: float = Query(0, ge=0),
+    max_stars: float = Query(5, ge=0),
+    min_ascents: int = Query(0, ge=0),
+    max_ascents: int = Query(999999, ge=0),
+    location: str = Query(...),
+    min_distance: float = Query(0, ge=0),
+    max_distance: float = Query(100, ge=0),
+    sort_by: SortBy = Query(SortBy.distance),
+    sort_order: SortOrder = Query(SortOrder.ascending),
+    start: int = 0,
+    limit: int = Query(20, le=100),
+):
+    pass
+
+
+@app.get(
     "/quickSearch",
     response_model=List[Union[Climb, Crag]],
     status_code=status.HTTP_200_OK,
 )
-def search_crags_and_climbs(
+def search_crags_sectors_and_climbs_by_name(
     text: str,
     limit: int = 16,
 ):
@@ -125,20 +317,3 @@ def search_crags_and_climbs(
         RankedImageItem(id=rank.item_id, rank=rank.rank)
         for rank in indexes[collection_id].rank(query_encoding, limit)
     ]
-
-
-@app.post(
-    "/climbs/search",
-    response_model=List[Climb],
-    status_code=status.HTTP_200_OK,
-)
-def advanced_search_for_climbs(
-    type: str,
-    grade: str,
-    stars: str,
-    ascents: str,
-    distance: str,
-    start: int = 0,
-    stop: int = 20,
-):
-    pass
